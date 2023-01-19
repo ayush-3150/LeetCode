06cc3ddf1e09f8d5282f31fa3bacb98621e1fd76
Ayush <ayushgandhi3105@gmail.com>
2023-01-19 17:15:53 +0530

    Traversal techniques with both recursive and iterative approach

--- Tree/Travaersals.java
+++ Tree/Travaersals.java
@@ -1,5 +1,10 @@
 package Leetcode.Tree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 public class Travaersals {
 
 
@@ -14,6 +19,8 @@ public class Travaersals {
         root.right.right = new Node(7);
         root.right.right.left = new Node(9);
         root.right.right.right = new Node(10);
+
+
         System.out.print("preorder ");
         preOrder(root);
         System.out.println();
@@ -22,7 +29,17 @@ public class Travaersals {
         System.out.println();
         System.out.print("inorder ");
         inOrder(root);
+        List<List<Integer>> list = levelOrder(root);
+        System.out.println();
+        System.out.println("Level Order" + list);
+        System.out.println();
+        List<Integer> ll = preOrderIterative(root);
+        System.out.println("preorder" + ll);
+        List<Integer> inOrder = inOrderIterative(root);
+        System.out.println("inorder" + inOrder);
 
+        List<Integer> postOrder = postOrderIterative(root);
+        System.out.println("postOrder" + postOrder);
     }
 
     public static void preOrder(Node node) {
@@ -50,6 +67,93 @@ public class Travaersals {
         postOrder(node.left);
         postOrder(node.right);
         System.out.print(node.data + " -> ");
+    }
+
+    public static List<List<Integer>> levelOrder(Node root) {
+        LinkedList<Node> ll = new LinkedList<>();
+        List<List<Integer>> list = new ArrayList<>();
+        if (root == null) return list;
+        ll.offer(root);
+
+        while (ll.size() > 0) {
+            int level = ll.size();
+            List<Integer> l = new ArrayList<>();
+            for (int i = 0; i < level; i++) {
+                if (ll.peek().left != null) ll.offer(ll.peek().left);
+                if (ll.peek().right != null) ll.offer(ll.peek().right);
+                l.add(ll.poll().data);
+            }
+            list.add(l);
+        }
+        return list;
+    }
+
+
+    public static List<Integer> preOrderIterative(Node node) {
+        Stack<Node> s = new Stack<>();
+        List<Integer> nodeValues = new ArrayList<>();
+        if (node == null) {
+            return nodeValues;
+        }
+        s.push(node);
+        while (!s.isEmpty()) {
+            int size = s.size();
+            node = s.pop();
+            if (node.right != null) s.push(node.right);
+            if (node.left != null) s.push(node.left);
+            nodeValues.add(node.data);
+        }
+        return nodeValues;
+    }
+
+    public static List<Integer> inOrderIterative(Node root) {
+        Stack<Node> s = new Stack<>();
+        List<Integer> inOrder = new ArrayList<>();
+        Node node = root;
+        while (true) {
+            if (node != null) {
+                s.push(node);
+                node = node.left;
+            } else {
+                if (s.isEmpty()) {
+                    break;
+                }
+                node = s.pop();
+                inOrder.add(node.data);
+                node = node.right;
+            }
+
+        }
+        return inOrder;
+    }
+
+    public static List<Integer> postOrderIterative(Node root) {
+
+        List<Integer> postOrder = new ArrayList<>();
+        Stack<Node> s1 = new Stack<>();
+        Stack<Node> s2 = new Stack<>();
+        if (root == null) {
+            return postOrder;
+        }
+
+        s1.push(root);
+        while (!s1.isEmpty()) {
+            root = s1.pop();
+            s2.push(root);
+            if (root.left != null) {
+                s1.push(root.left);
+            }
+            if (root.right != null) {
+                s1.push(root.right);
+            }
+        }
+
+        while (!s2.isEmpty()) {
+            postOrder.add(s2.pop().data);
+        }
+
+
+        return postOrder;
     }
 }
 
