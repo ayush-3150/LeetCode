4256c599a0f85a7595bcd5992812904b68c5a23d
Ayush <ayushgandhi3105@gmail.com>
2023-02-15 14:41:42 +0530

    Bubble Sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -10,16 +10,27 @@ public class BubbleSort {
         }
     }
 
+    //Time complexity: best case-o(n) worst case o(n2)
+    //space complexity:o(1)
     public static void bubbleSort(int arr[]) {
         int n = arr.length;
+        boolean swapped;
+
+
         for (int i = 0; i < n; i++) {
+            swapped = false;
+
+            //check if j-1 element is greater than j if yes then swap both number
+            //every pass largest element of particular slot of an array will be at the end
             for (int j = 1; j < n - i; j++) {
                 if (arr[j] < arr[j - 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j - 1];
                     arr[j - 1] = temp;
+                    swapped = true;
                 }
             }
+            if (!swapped) break;
         }
     }
 }
